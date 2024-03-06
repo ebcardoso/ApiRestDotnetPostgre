c8ad911822e500302a89746f5a881b2821dec868
Breno Cardoso <elisiobreno@gmail.com>
2024-03-06 01:44:19 -0300

    Adding timestampps for Model User

--- Api/Domain/Models/User.cs
+++ Api/Domain/Models/User.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiRestPostgre.Api.Domain.Models;
 
-public class User
+public class User : BaseModel
 {
   public int Id { get; private set; }
   
