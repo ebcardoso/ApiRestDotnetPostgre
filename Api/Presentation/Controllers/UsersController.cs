6597f55d42e25ec90917398acf57e64a470e4280
Breno Cardoso <elisiobreno@gmail.com>
2024-03-09 15:41:40 -0300

    Endpoint create user

--- Api/Presentation/Controllers/UsersController.cs
+++ Api/Presentation/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ApiRestPostgre.Api.Application.ServiceInterfaces;
 using ApiRestPostgre.Api.Application.DTO;
+using ApiRestPostgre.Api.Presentation.Responses.Auth;
 
 namespace ApiRestPostgre.Api.Presentation.Controllers;
 
@@ -9,16 +10,32 @@ namespace ApiRestPostgre.Api.Presentation.Controllers;
 public class UsersController : ControllerBase
 {
   private readonly IUsersServices _usersServices;
+  private readonly IAuthServices _authServices;
 
-  public UsersController(IUsersServices usersServices)
+  public UsersController(IUsersServices usersServices, IAuthServices authServices)
   {
     _usersServices = usersServices;
+    _authServices = authServices;
   }
 
   [HttpGet]
-  public async Task<IEnumerable<UserDTO>> GetBrands()
+  public async Task<IEnumerable<UserDTO>> GetUsers()
   {
     var modelsDTO = await _usersServices.GetAllAsync();
     return modelsDTO;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<UserDTO>> CreateUser(UserDTO modelDTO)
+  {
+    var emailExists = await _authServices.UserExists(modelDTO.Email);
+    if (emailExists)
+    {
+      var response = new ErrorResponse{ Message = "This email already exists."};
+      return BadRequest(response);
+    }
+
+    var model = await _usersServices.Create(modelDTO);
+    return model;
   }
 }
